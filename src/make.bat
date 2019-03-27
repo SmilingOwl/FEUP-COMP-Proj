27838275d1130474a078941b8347410d4048cb75
FernandoJAlves <up201605270@fe.up.pt>
2019-03-27 14:51:27 +0000

    Started redoing the AST

--- src/make.bat
+++ src/make.bat
@@ -2,6 +2,8 @@
 ECHO ============================
 call jjtree Project.jjt
 ECHO ============================
+cd ASTFiles
 call javacc Project.jj
 ECHO ============================
+cd ..
 call javac *.java
